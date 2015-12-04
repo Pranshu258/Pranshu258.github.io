4fbf5bc79f5516b13fe1d01e845cc7fadbcc6140
Pranshu <pranshug258@outlook.com>
2015-12-04 11:58:19 +0530

    Homepage 3.0

--- includes/js/script.js
+++ includes/js/script.js
@@ -13,15 +13,20 @@ $(
 		});
 })
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-$('#submenu1').hide();
-$('#submenu1_toggle').bind('click', function() {
-	if ($('#submenu2')) {$('#submenu2').slideUp();};
-	if ($('#submenu')) { $('#submenu').slideUp(); };
-	$('#submenu1').slideToggle();
-});
-$('#submenu2').hide();
-$('#submenu2_toggle').bind('click', function() {
-	if ($('#submenu1')) {$('#submenu1').slideUp();};
-	if ($('#submenu')) { $('#submenu').slideUp(); };
-	$('#submenu2').slideToggle();
+
+$(function() {
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 20
+        }, 500); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
 });
