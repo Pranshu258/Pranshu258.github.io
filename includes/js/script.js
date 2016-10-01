358ce76b64745cee226fa5e7c94ff2080a2a18ad
Pranshu Gupta <pranshug258@outlook.com>
2016-10-02 05:17:43 +0530

    update scroll behavior

--- includes/js/script.js
+++ includes/js/script.js
@@ -12,83 +12,10 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 20
         }, 500); // The number here represents the speed of the scroll in milliseconds
         return false;
       }
-    }
-  });
-
-  $('#proj1').slideUp();
-  $('#proj1toggle').bind('click', function() {
-    $('#proj1').slideDown();
-    var target = $('#proj1');
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - 20
-      }, 500); // The number here represents the speed of the scroll in milliseconds
-      return false;
-    }     
-  });
-
-  $('#proj2').slideUp();
-  $('#proj2toggle').bind('click', function() {
-    $('#proj2').slideDown();
-    var target = $('#proj2');
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - 20
-      }, 500); // The number here represents the speed of the scroll in milliseconds
-      return false;
-    }     
-  });
-
-  $('#proj3').slideUp();
-  $('#proj3toggle').bind('click', function() {
-    $('#proj3').slideDown();
-    var target = $('#proj3');
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - 20
-      }, 500); // The number here represents the speed of the scroll in milliseconds
-      return false;
-    }
-  });
-
-  $('#proj1close').bind('click', function() {
-    $('#proj1').slideUp();
-    var target = $('#projects');
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - 20
-      }, 500); // The number here represents the speed of the scroll in milliseconds
-      return false;
-    }
-  });
-  $('#proj2close').bind('click', function() {
-    $('#proj2').slideUp();
-    var target = $('#projects');
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - 20
-      }, 500); // The number here represents the speed of the scroll in milliseconds
-      return false;
-    }
-  });
-  $('#proj3close').bind('click', function() {
-    $('#proj3').slideUp();
-    var target = $('#projects');
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top - 20
-      }, 500); // The number here represents the speed of the scroll in milliseconds
-      return false;
     }
   });
 
@@ -101,10 +28,10 @@ $(window).scroll(function(event){
    var st = $(this).scrollTop();
    if (st > lastScrollTop){
    		//Scroll down
-       	navbar.css('padding', '8px');
+       	navbar.css('padding', '4px');
    } else {
    		//Scroll up
-      	navbar.css('padding', '15px');
+      	navbar.css('padding', '8px');
    }
    lastScrollTop = st;
 });
