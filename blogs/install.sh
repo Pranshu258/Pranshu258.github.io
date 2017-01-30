ffb0cb443c7203b59f132da114ed12509a8fcbb4
Pranshu Gupta <pranshug258@outlook.com>
2017-01-30 22:01:03 +0530

    fixed a opencv installation command

--- blogs/install.sh
+++ blogs/install.sh
@@ -74,6 +74,7 @@ sudo apt install libatlas-base-dev gfortran
 git clone https://github.com/Itseez/opencv.git
 cd opencv
 git checkout 3.1.0
+cd ../
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv_contrib
 git checkout 3.1.0
