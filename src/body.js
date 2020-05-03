46958229a8780c52f0ac42f1c5d16a6cd07ed1a6
Pranshu Gupta <pranshug258@outlook.com>
2020-05-03 16:23:19 -0400

    removed photography section

--- src/body.js
+++ src/body.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Route, Link } from "react-router-dom";
-
 import { blogList } from './data/blogs'
 import { projectList } from './data/projects'
 
 import './styles/fonts.css';
 import './styles/body.css';
 
-import art2 from './images/img2.jpg';
+import art1 from './images/img1.jpg';
 import art3 from './images/img12.jpg';
 import art4 from './images/img13.jpg';
 import art5 from './images/img14.jpg';
 
-import photo1 from './images/photo1.jpg';
-import photo2 from './images/photo2.jpg';
-import photo3 from './images/photo3.jpg';
-import photo4 from './images/photo4.jpg';
 
 import About from "./about";
 
@@ -99,51 +94,23 @@ export default class Body extends React.Component {
                         <div className="col-sm-12">
                             <div className="row">
                                 <div className="col-sm-3">
+                                    <img alt="" src={art1} className="img-fluid" style={bannerStyle}></img>
+                                </div>
+                                <div className="col-sm-3">
+                                    <img alt="" src={art3} className="img-fluid" style={bannerStyle}></img>
+                                </div>
+                                <div className="col-sm-3">
                                     <img alt="" src={art4} className="img-fluid" style={bannerStyle}></img>
                                 </div>
                                 <div className="col-sm-3">
                                     <img alt="" src={art5} className="img-fluid" style={bannerStyle}></img>
                                 </div>
-                                <div className="col-sm-3">
-                                    <img alt="" src={art2} className="img-fluid" style={bannerStyle}></img>
-                                </div>
-                                <div className="col-sm-3">
-                                    <img alt="" src={art3} className="img-fluid" style={bannerStyle}></img>
-                                </div>
                             </div>
                             <br></br>
                             <br></br>
                         </div>
                     </div>
-                    <hr></hr>
-                    <div className="row" id="photography">
-                        <div className="col-sm-12">
-                            <br></br><br></br>
-                            <h1 className="roboto">
-                                PHOTOGRAPHY
-                            </h1>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <div className="row">
-                                <div className="col-sm-3">
-                                    <img alt="" src={photo1} className="img-fluid" style={bannerStyle}></img>
-                                </div>
-                                <div className="col-sm-3">
-                                    <img alt="" src={photo2} className="img-fluid" style={bannerStyle}></img>
-                                </div>
-                                <div className="col-sm-3">
-                                    <img alt="" src={photo3} className="img-fluid" style={bannerStyle}></img>
-                                </div>
-                                <div className="col-sm-3">
-                                    <img alt="" src={photo4} className="img-fluid" style={bannerStyle}></img>
-                                </div>
-                            </div>
-                            <br></br>
-                            <br></br>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         );
