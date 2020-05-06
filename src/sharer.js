6d3111ce3bc78f2bb812d6485d2942d908ef8703
Pranshu Gupta <pranshug258@outlook.com>
2020-05-06 19:46:08 -0400

    added sharer component

---
+++ src/sharer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './styles/sharer.css';
+
+export default class Sharer extends React.Component {
+    render() {
+        
+        return (
+            <ul>
+                <li>
+                    <a style={{color:"black"}} href={"https://twitter.com/share?url=" + encodeURIComponent(this.props.link)}>
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a  style={{color:"black"}} href={"https://www.linkedin.com/sharing/share-offsite/?url=" + encodeURIComponent(this.props.link)}>
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                </li>
+                <li>
+                    <a  style={{color:"black"}} href={"https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(this.props.link)}>
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                </li>
+            </ul>
+        );
+    }
+}
