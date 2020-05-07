336f45ad801f7abf2ab7e1e0558508f0f5751873
Pranshu Gupta <pranshug258@outlook.com>
2020-05-06 20:17:16 -0400

    added blank targets for sharer

--- src/sharer.js
+++ src/sharer.js
@@ -7,17 +7,17 @@ export default class Sharer extends React.Component {
         return (
             <ul>
                 <li>
-                    <a style={{color:"black"}} href={"https://twitter.com/share?url=" + encodeURIComponent(this.props.link)}>
+                    <a target="_blank" style={{color:"black"}} href={"https://twitter.com/share?url=" + encodeURIComponent(this.props.link)}>
                         <i className="fab fa-twitter"></i>
                     </a>
                 </li>
                 <li>
-                    <a  style={{color:"black"}} href={"https://www.linkedin.com/sharing/share-offsite/?url=" + encodeURIComponent(this.props.link).replace("blog", "%3Fp%3Dblog")}>
+                    <a target="_blank" style={{color:"black"}} href={"https://www.linkedin.com/sharing/share-offsite/?url=" + encodeURIComponent(this.props.link).replace("blog", "%3Fp%3Dblog")}>
                         <i className="fab fa-linkedin"></i>
                     </a>
                 </li>
                 <li>
-                    <a  style={{color:"black"}} href={"https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(this.props.link)}>
+                    <a target="_blank" style={{color:"black"}} href={"https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(this.props.link)}>
                         <i className="fab fa-facebook"></i>
                     </a>
                 </li>
