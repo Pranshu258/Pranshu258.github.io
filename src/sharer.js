fbe76714f3b8a06c06e961536f8a58ba4b7b495f
Pranshu Gupta <pranshug258@outlook.com>
2020-05-12 05:12:15 -0400

    modified sharer to use crawlable links

--- src/sharer.js
+++ src/sharer.js
@@ -7,7 +7,7 @@ export default class Sharer extends React.Component {
         return (
             <ul>
                 <li>
-                    <a target="_blank" rel="noopener noreferrer" style={{color:"black"}} href={"https://twitter.com/share?url=" + encodeURIComponent(this.props.link)}>
+                    <a target="_blank" rel="noopener noreferrer" style={{color:"black"}} href={"https://twitter.com/share?url=" + encodeURIComponent(this.props.link).replace("blog", "%3Fp%3Dblog")}>
                         <i className="fab fa-twitter"></i>
                     </a>
                 </li>
@@ -17,7 +17,7 @@ export default class Sharer extends React.Component {
                     </a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noopener noreferrer" style={{color:"black"}} href={"https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(this.props.link)}>
+                    <a target="_blank" rel="noopener noreferrer" style={{color:"black"}} href={"https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(this.props.link).replace("blog", "%3Fp%3Dblog")}>
                         <i className="fab fa-facebook"></i>
                     </a>
                 </li>
