8f3562127ce3dc0ecff34c333d13f33d8c9f1d5f
Pranshu Gupta <pranshug258@outlook.com>
2020-05-15 04:58:21 -0400

    fixed the stupid heading issue in ovac

--- src/blogs/ovac.js
+++ src/blogs/ovac.js
@@ -22,58 +22,59 @@ export default class Ovac extends React.Component {
                 <p>
                     Virtualization is a mechanism to share hardware resources amongst multiple operating system instances on the same machine. These operating system instances are called Virtual Machines and are isolated from one another. This reduces the investment required in hardware resources, as the same hardware can now be shared by multiple VMs allowing high density deployments on a single server.
                     Virtualization is made possible by a thin layer of software called a Hypervisor that decouples the virtual machines from the host and dynamically allocates computing resources to each virtual machine as needed.
-                    </p>
+                </p>
                 <p>
                     The only responsibility of the host operating system is to run the Hypervisor itself, no app deployments are usually done on the host OS. However, each of the OS instances incur licensing and maintenance costs. All the system calls and resources access requests by the VMs first go through the Hypervisor and the host OS, resulting in some overhead. VMWare came up with bare metal hypervisor which runs directly on the hardware, without any host OS, eliminating this overhead.
-                    </p>
+                </p>
                 <p>
                     <b><i>VMware ESXi is a purpose-built bare-metal hypervisor that installs directly onto a physical server. With direct access to and control of underlying resources, ESXi is more efficient than hosted architectures and can effectively partition hardware to increase consolidation ratios and cut costs for our customers. - VMWare</i></b>
                 </p>
                 <h3 className="roboto">Types of Virtualization</h3>
                 <p>
                     There are four types of virtualization – Paravirtualization, Hardware Assisted Virtualization, Full Virtualization and Hybrid Virtualization.
-                    </p>
+                </p>
                 <p>
                     Paravirtualization is virtualization in which the guest operating system (the one being virtualized) is aware that it is a guest and accordingly has drivers that, instead of issuing hardware commands, simply issue commands directly to the host operating system. This also includes memory and thread management as well, which usually require unavailable privileged instructions in the processor.
-                    </p>
+                </p>
                 <p>
                     Full Virtualization is virtualization in which the guest operating system is not aware that it is running on virtual hardware and issues hardware commands directly which are handled by the underlying hypervisor software running on the host operating system.
-                    </p>
+                </p>
                 <p>
                     Hardware Assisted Virtualization is virtualization in which the microprocessor architecture supports special instructions to facilitate virtualization. These instructions might allow a virtual context to be setup so that the guest can execute privileged instructions directly on the processor without affecting the host.
-                    </p>
+                </p>
                 <p>
                     Hybrid Virtualization is a mix of Paravirtualization and full virtualization where for privileged kernel commands, full virtualization is used, and for other tasks like IO, the guest OS directly issues commands to the host OS using the special drivers. Hybrid VMs often perform better than fully virtualized VMs.
-                    </p>
+                </p>
                 <h3 className="roboto">Open Virtualization Format</h3>
                 <p>
                     Open Virtualization Format is a standard that allows compliant virtual machine images to be interoperable and migratable across different vendors such as Azure, AWS, VMWare and VirtualBox.
-                    </p>
+                </p>
                 <p>
                     OVF is a packaging format for virtual appliances. Once installed, an OVF package adds to the user’s infrastructure a self-contained, self-consistent, software application that provides a particular service or services. For example, an OVF package might contain a fully functional and tested web-server, database, and OS combination, such as a LAMP stack (Linux + Apache + MySQL + PHP), or it may contain a virus checker, including its update software, spyware detector, etc.
-                    </p>
+                </p>
                 <p>
                     The OVF specification describes a hypervisor-neutral, efficient, extensible, and open format for the packaging and distribution of virtual appliances composed of one or more virtual systems. The OVF specification promotes customer confidence through the collaborative development of common standards for portability and interchange of virtual systems between different vendors’ virtualization platforms.
-                    </p>
+                </p>
                 <p>
-                    For more information on OVF, see the white 
+                    For more information on OVF, see the white
                     <a href="https://www.dmtf.org/sites/default/files/standards/documents/DSP2017_2.0.0.pdf"> paper</a>
                 </p>
                 <h3 className="roboto">Limitations of Virtualization</h3>
                 <p>
                     Virtualization has its advantages but there are limitations too, some of them are as follows:
-                    </p>
-                <ol>
-                    <li> No concept incremental versioning or layers in virtualization, that means if there is a patch or update, the full VM image needs be downloaded and deployed again</li>
-                    <li> No centralized repository for VM images, reuse opportunities are minimal</li>
-                    <li> Not suitable for modern DevOps workflow</li>
-                    <li> Hypervisor server does not support clusters by itself. An external load balancer is needed</li>
-                    <li> Deploying web/micro services on VMs is costly because of low density deployments One way is to deploy multiple services on the same host, but this is bad practice, because it makes maintenance and failure detection difficult</li>
-                    <li> Provisioning and bootstrapping VMs is time consuming so it is not suitable for real time workloads For example, dynamic scaling in real time will be very difficult because creating and deploying new VMs takes time</li>
-                    <li> Vendor specific toolset is required for administration of virtual environments</li>
-                    <li> VMs are costly because of OS licensing and low-density utilization of underlying hardware</li>
-                    <li> VMs are difficult to migrate unless they were created using OVF</li>
-                </ol>
+                    <ol>
+                        <li> No concept incremental versioning or layers in virtualization, that means if there is a patch or update, the full VM image needs be downloaded and deployed again</li>
+                        <li> No centralized repository for VM images, reuse opportunities are minimal</li>
+                        <li> Not suitable for modern DevOps workflow</li>
+                        <li> Hypervisor server does not support clusters by itself. An external load balancer is needed</li>
+                        <li> Deploying web/micro services on VMs is costly because of low density deployments One way is to deploy multiple services on the same host, but this is bad practice, because it makes maintenance and failure detection difficult</li>
+                        <li> Provisioning and bootstrapping VMs is time consuming so it is not suitable for real time workloads For example, dynamic scaling in real time will be very difficult because creating and deploying new VMs takes time</li>
+                        <li> Vendor specific toolset is required for administration of virtual environments</li>
+                        <li> VMs are costly because of OS licensing and low-density utilization of underlying hardware</li>
+                        <li> VMs are difficult to migrate unless they were created using OVF</li>
+                    </ol>
+                </p>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
                 <h2 className="roboto">Docker Containers</h2>
                 <p>
                     Containers are isolated execution environments which share the operating system resources. Virtualization at the operating system level can be regarded as containerization. The operating system takes care of providing the virtual environments where the applications run with isolation as if each application was running on a separate OS instance.
