ee2194fb1cb07f5a75d97a834bda1b59cdbf772c
Pranshu Gupta <pranshug258@outlook.com>
2020-08-22 12:55:08 -0700

    moved artworks data to a data file instead of the component

--- src/artworks.js
+++ src/artworks.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { Route } from "react-router-dom";
 
 import './styles/fonts.css';
 import './styles/body.css';
 import './styles/blog.css';
 
-import art1 from './images/img1.jpg';
-import art2 from './images/img2.jpg';
-import art3 from './images/img3.jpg';
-import art4 from './images/img4.jpg';
-import art5 from './images/img5.jpg';
-import art6 from './images/img6.jpg';
-import art7 from './images/img7.jpg';
-import art8 from './images/img12.jpg';
-import art9 from './images/img13.jpg';
-import art10 from './images/img14.jpg';
+import { artList } from './data/artworks'
 
 import banner from './images/banner.png';
 
@@ -37,156 +27,25 @@ export default class Artworks extends React.Component {
                             <div className="col-lg-9">
                                 <h1 className="big"><b>Artworks</b></h1>
                                 <br></br>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art4} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Betta</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art1} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Dancing Beauty</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art3} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Toucan</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art5} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Watchtower</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art6} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>The Stare</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art7} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Dancing Beauty II</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art8} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>The Night King</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art9} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Touka: The Rabbit</b>
-                                            <br></br>
-                                            Watercolor on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art10} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Raccoons</b>
-                                            <br></br>
-                                            Color Pencil on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img alt="" src={art2} className="img-fluid" style={bannerStyle}></img>
-                                    </div>
-                                    <div className="col">
-                                        <p>
-                                            <br></br>
-                                            <b>Anne Hathaway</b>
-                                            <br></br>
-                                            Graphite on Paper
-                                            <br></br>
-                                            Original (12'x16')
-                                        </p>
-                                    </div>
-                                </div>
+                                {
+                                    artList.map((object, i) =>
+                                        <div className="row">
+                                            <div className="col">
+                                                <img alt="" src={object.source} className="img-fluid" style={bannerStyle}></img>
+                                            </div>
+                                            <div className="col">
+                                                <p>
+                                                    <br></br>
+                                                    <b>{object.title}</b>
+                                                    <br></br>
+                                                    {object.medium}
+                                                    <br></br>
+                                                    {object.size}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    )
+                                }
                             </div>
                             <div className="col-lg-3">
                                 <br></br><br></br>
---
+++ src/data/artworks.js
@@ -0,0 +1,73 @@
+import art1 from '../images/img1.jpg';
+import art2 from '../images/img2.jpg';
+import art3 from '../images/img3.jpg';
+import art4 from '../images/img4.jpg';
+import art5 from '../images/img5.jpg';
+import art6 from '../images/img6.jpg';
+import art7 from '../images/img7.jpg';
+import art8 from '../images/img12.jpg';
+import art9 from '../images/img13.jpg';
+import art10 from '../images/img14.jpg';
+
+export let artList = [
+    {
+        source: art4,
+        title: "Betta", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art1,
+        title: "Dancing Beauty", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art3,
+        title: "Toucan", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art5,
+        title: "Watchtower", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art6,
+        title: "The Stare", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art7,
+        title: "Dancing Beauty II", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art8,
+        title: "The Night King", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art9,
+        title: "Touka: The Rabbit", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art10,
+        title: "Raccoons", 
+        medium: "Color Pencil on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: art2,
+        title: "Anne Hathaway", 
+        medium: "Graphite on Paper",
+        size: "Original (12'x16')"
+    }
+]
