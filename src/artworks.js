c96d3ba585ff59d9c057781ae7ba6d462bddfed6
Pranshu Gupta <pranshug258@outlook.com>
2021-01-22 02:25:40 -0800

    added more artwork

--- src/artworks.js
+++ src/artworks.js
@@ -17,40 +17,40 @@ export default class Artworks extends React.Component {
             margin: "50px 0 50px 0",
         }
         var bannerStyle = {
-            margin: "20px 0 20px 0",
+            margin: "0 40px 0 0",
+            height: "420px"
+        }
+        var wrapStyle = {
+            flexWrap: "wrap",
+            display: "flex"
         }
         return (
             <div className="content">
                 <div className="container">
                     <div className="row-fluid" style={globalStyle}>
                         <div className="row">
-                            <div className="col-lg-9">
+                            <div className="col-lg-12">
                                 <h1 className="big"><b>Artworks</b></h1>
                                 <br></br>
-                                {
-                                    artList.map((object, i) =>
-                                        <div className="row">
-                                            <div className="col">
-                                                <img alt="" src={object.source} className="img-fluid" style={bannerStyle}></img>
-                                            </div>
-                                            <div className="col">
+                                <div style={wrapStyle}>
+                                    {
+                                        artList.map((object, i) =>
+                                            <span>
                                                 <p>
-                                                    <br></br>
+                                                    <img alt="" src={object.source} className="img-fluid" style={bannerStyle}></img>
+                                                </p>
+                                                <p>
                                                     <b>{object.title}</b>
                                                     <br></br>
                                                     {object.medium}
                                                     <br></br>
                                                     {object.size}
                                                 </p>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            </div>
-                            <div className="col-lg-3">
-                                <br></br><br></br>
-                                <img alt="" src={banner} className="img-fluid" style={bannerStyle}></img>
-                                <br></br><br></br>
+                                                <br></br>
+                                            </span>
+                                        )
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/data/artworks.js
+++ src/data/artworks.js
@@ -8,8 +8,43 @@ import art7 from '../images/img7.jpg';
 import art8 from '../images/img12.jpg';
 import art9 from '../images/img13.jpg';
 import art10 from '../images/img14.jpg';
+import nefertiti from '../images/IMG_5742.jpg';
+import aphrodite from '../images/IMG_5746.jpg';
+import kathak1 from '../images/IMG_6029.jpg';
+import kathak2 from '../images/IMG_6043.jpg';
+import squirrel from '../images/IMG_6281.jpg';
+import orca from '../images/IMG_6283.jpg';
+import diana from '../images/IMG_6337.jpg';
+import geralt from '../images/IMG_6378.jpg';
+import nyc from '../images/IMG_6950.jpg';
+import medhaR from '../images/IMG_6958.jpg';
+import sanjanaS from '../images/IMG_6977.jpg';
 
 export let artList = [
+    {
+        source: sanjanaS,
+        title: "The Lady in Pink", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: medhaR,
+        title: "The Lady in Red", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: geralt,
+        title: "Geralt of Rivia", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: nefertiti,
+        title: "Queen Nefertiti of Egypt", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
     {
         source: art4,
         title: "Betta", 
@@ -23,8 +58,50 @@ export let artList = [
         size: "Original (12'x16')"
     },
     {
+        source: diana,
+        title: "The Lady in White", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: kathak1,
+        title: "Kathak Dancer 1", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: kathak2,
+        title: "Kathak Dancer 2", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: aphrodite,
+        title: "Goddess Aphrodite of Greece", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
         source: art3,
         title: "Toucan", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: squirrel,
+        title: "Giant Malabar Squirrel", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: orca,
+        title: "Killer Whales", 
+        medium: "Watercolor on Paper",
+        size: "Original (12'x16')"
+    },
+    {
+        source: nyc,
+        title: "Towards Liberty Island", 
         medium: "Watercolor on Paper",
         size: "Original (12'x16')"
     },
