70d7e01867761872c7466892a9612b3cba6e2356
Pranshu Gupta <pranshug258@outlook.com>
2022-04-20 00:20:40 -0700

    updated tags

--- src/data/publications.js
+++ src/data/publications.js
@@ -3,10 +3,8 @@ export let publicationList = [
         name: "Impacts of school shooter drills on the psychological well-being of American K-12 school communities",
         description: "Applied machine learning and interrupted time series analysis to 54 million social media posts in 114 schools spanning 33 states to study psychological impacts of school shooter drills. Showed that anxiety, stress, and depression increased by 39–42% following the drills, accompanied by an increase in civic engagement up to 10–106%",
         tags: [
-            "Humanities and Social Sciences",
-            "Nature Journal",
-            "Data Analysis",
-            "Third Author"
+            "Nature HSSC, 2021",
+            "3rd Author"
         ],
         link: "https://www.nature.com/articles/s41599-021-00993-6"
     },
@@ -14,10 +12,9 @@ export let publicationList = [
         name: "A social media study on demographic differences in perceived job satisfaction",
         description: "Applied machine learning techniques to assess job satisfaction by leveraging large-scale social media data. Current methodologies are limited in capturing subjective differences in expectations, needs, and values at work, and they do not lay emphasis on demographic differences, which may impact people's perceptions of job satisfaction. This study focuses on demographic diferences in perceived job satisfaction by geography, sex, and race in the U.S",
         tags: [
-            "Human Centered Computing",
-            "Data Analysis",
-            "Social Media",
-            "Fourth Author"
+            "CSCW (PACM HCI)",
+            "Presented at CSCW 2021",
+            "4rth Author"
         ],
         link: "https://www.researchgate.net/profile/Koustuv-Saha-2/publication/349124174_A_Social_Media_Study_on_Demographic_Differences_in_Perceived_Job_Satisfaction/links/6021d3cc299bf1cc26b24a15/A-Social-Media-Study-on-Demographic-Differences-in-Perceived-Job-Satisfaction.pdf"
     },
@@ -25,10 +22,9 @@ export let publicationList = [
         name: "Person-centered predictions of psychological constructs with social media contextualized by multimodal sensing",
         description: "Personalized predictions have shown promises in various disciplines but they are fundamentally constrained in their ability to generalize across individuals. In contrast, generalized models capture normative behaviors between individuals but lack precision in predicting individual outcomes. This paper aims to balance the tradeoff between one-for-each and one-for-all models by clustering individuals on mutable behaviors and conducting cluster-specific predictions of psychological constructs in a multimodal sensing dataset of 754 individuals.",
         tags: [
-            "Human Centered Computing",
-            "Data Analysis",
-            "Social Media",
-            "Fourth Author"
+            "PACM IMWUT, 2021",
+            "Presented at UbiComp 2021",
+            "4rth Author"
         ],
         link: "https://dl.acm.org/doi/pdf/10.1145/3448117"
     }
