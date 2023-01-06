d8cddf5b64784cb16f282efe5f39e04040f795c6
Pranshu Gupta <pranshug258@outlook.com>
2023-01-05 23:39:48 -0800

    updated spotify link

--- src/footer.js
+++ src/footer.js
@@ -15,7 +15,7 @@ export default class Footer extends React.Component {
                             <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://www.linkedin.com/in/pranshug258"><i className="fab fa-linkedin socialIcon"></i></a>
                             <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://www.goodreads.com/prang"><i className="fab fa-goodreads socialIcon"></i></a>
                             <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://www.instagram.com/pranshu.paints/"><i className="fab fa-instagram socialIcon"></i></a>                   
-                            <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://open.spotify.com/playlist/3lciV1bfpnnGa2JzSfntGJ?si=5b549b8049a9413f"><i className="fab fa-spotify socialIcon"></i></a>
+                            <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://open.spotify.com/user/pranshugupta?si=a2369fa3eaf24985"><i className="fab fa-spotify socialIcon"></i></a>
                             <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://github.com/Pranshu258"><i className="fab fa-github socialIcon"></i></a>
                             <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://in.pinterest.com/pranshugupta258/boards/"><i className="fab fa-pinterest socialIcon"></i></a>
                             <a target="_blank" rel="noopener noreferrer" style={{color:"white"}} href="https://www.facebook.com/pranshug258"><i className="fab fa-facebook socialIcon"></i></a>
