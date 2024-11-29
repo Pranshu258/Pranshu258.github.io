7cb979c3ed62baef37294ff67a2db1a599d94c4f
Pranshu Gupta <pranshug258@outlook.com>
2024-11-29 02:40:49 -0800

    updated artworks page

--- src/artworks.js
+++ src/artworks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { InstagramEmbed } from 'react-social-media-embed';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import './styles/fonts.css';
 import './styles/body.css';
@@ -15,12 +18,7 @@ export default class Artworks extends React.Component {
             margin: "50px 0 50px 0",
         }
         var bannerStyle = {
-            margin: "0 40px 0 0",
-            height: "420px"
-        }
-        var wrapStyle = {
-            flexWrap: "wrap",
-            display: "flex"
+            margin: "0 0 20px 0",
         }
         return (
             <div className="content">
@@ -30,25 +28,41 @@ export default class Artworks extends React.Component {
                             <div className="col-lg-12">
                                 <h1 className="big"><b>Artworks</b></h1>
                                 <br></br>
-                                <div style={wrapStyle}>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <div style={{ display: 'flex', justifyContent: 'left' }}>
+                                    <InstagramEmbed url="https://www.instagram.com/p/CySjN5TOvxF/" />
+                                </div>
+                                <br></br>
+                            </div>
+                            <div className="col-md-8">
+                                <p>
+                                    Art is my soul's expression, a journey through the vibrant hues and delicate strokes of watercolor, with gouache adding depth and brilliance. My landscapes are a heartfelt homage to the breathtaking Pacific Northwest, each piece a testament to the awe and wonder I feel in nature's embrace. My passion extends to movies and video games as well, where I channel my admiration into fan art that resonates with fellow enthusiasts. Through my art, I strive to capture not just the scene, but the very essence and emotion it evokes within me.
+                                </p>
+                                <h2 className='calligraffitti'><b>- Pranshu Gupta</b></h2>
+                                <br></br>
+                                <Carousel 
+                                    showThumbs={true} 
+                                    showStatus={false}
+                                    infiniteLoop={true} 
+                                    useKeyboardArrows={true} 
+                                    autoPlay={true}
+                                    showIndicators={false}
+                                    swipeable={true}
+                                    showArrows={false}
+                                >
                                     {
                                         artList.map((object, i) =>
-                                            <span>
-                                                <p>
-                                                    <img alt="" src={object.source} className="img-fluid" style={bannerStyle}></img>
-                                                </p>
-                                                <p>
-                                                    <b>{object.title}</b>
-                                                    <br></br>
-                                                    {object.medium}
-                                                    <br></br>
-                                                    {object.size}
-                                                </p>
-                                                <br></br>
-                                            </span>
+                                            <div key={i}>
+                                                <img src={object.source} alt={`Thumbnail ${i}`} />
+                                                <br></br><br></br>
+                                                <p style={{ textAlign: 'left' }}>{object.description}</p>
+                                            </div>
                                         )
                                     }
-                                </div>
+                                </Carousel>
                             </div>
                         </div>
                     </div>
--- src/data/artworks.js
+++ src/data/artworks.js
@@ -1,150 +1,33 @@
-import art1 from '../images/img1.jpg';
-import art2 from '../images/img2.jpg';
-import art3 from '../images/img3.jpg';
-import art4 from '../images/img4.jpg';
-import art5 from '../images/img5.jpg';
-import art6 from '../images/img6.jpg';
-import art7 from '../images/img7.jpg';
-import art8 from '../images/img12.jpg';
-import art9 from '../images/img13.jpg';
-import art10 from '../images/img14.jpg';
-import nefertiti from '../images/IMG_5742.jpg';
-import aphrodite from '../images/IMG_5746.jpg';
-import kathak1 from '../images/IMG_6029.jpg';
-import kathak2 from '../images/IMG_6043.jpg';
-import squirrel from '../images/IMG_6281.jpg';
-import orca from '../images/IMG_6283.jpg';
-import diana from '../images/IMG_6337.jpg';
-import geralt from '../images/IMG_6378.jpg';
-import nyc from '../images/IMG_6950.jpg';
-import medhaR from '../images/IMG_6958.jpg';
-import sanjanaS from '../images/IMG_6977.jpg';
+import art01 from '../images/art/IMG_3748.jpg';
+import art02 from '../images/art/IMG_3476.jpg';
+import art03 from '../images/art/IMG_3725.jpg';
+import art04 from '../images/art/IMG_3730.jpg';
+import art05 from '../images/art/IMG_3918.jpg'; 
+import art06 from '../images/art/IMG_4609.jpg';
+import art07 from '../images/art/IMG_6424.jpg'; 
+import art08 from '../images/art/IMG_6977.jpg';
+import art09 from '../images/art/IMG_7411.jpg';
+import art10 from '../images/art/IMG_8235.jpg';
 
 export let artList = [
-    {
-        source: sanjanaS,
-        title: "The Lady in Pink", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
+    { 
+        source: art01,
+        description: "A watercolor painting of Tahoma (Mount Rainier) in the Pacific Northwest, from an aerial perspective. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
     },
-    {
-        source: medhaR,
-        title: "The Lady in Red", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
+    { 
+        source: art02,
+        description: "A watercolor painting of Tahoma (Mount Rainier) in the Pacific Northwest, from an aerial perspective. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
     },
-    {
-        source: geralt,
-        title: "Geralt of Rivia", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
+    { 
+        source: art03,
+        description: "A watercolor painting of Tahoma (Mount Rainier) in the Pacific Northwest, from an aerial perspective. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
     },
-    {
-        source: nefertiti,
-        title: "Queen Nefertiti of Egypt", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
+    { 
+        source: art04,
+        description: "A watercolor painting of Tahoma (Mount Rainier) in the Pacific Northwest, from an aerial perspective. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
     },
-    {
-        source: art4,
-        title: "Betta", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art1,
-        title: "Dancing Beauty", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: diana,
-        title: "The Lady in White", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: kathak1,
-        title: "Kathak Dancer 1", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: kathak2,
-        title: "Kathak Dancer 2", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: aphrodite,
-        title: "Goddess Aphrodite of Greece", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art3,
-        title: "Toucan", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: squirrel,
-        title: "Giant Malabar Squirrel", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: orca,
-        title: "Killer Whales", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: nyc,
-        title: "Towards Liberty Island", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art5,
-        title: "Watchtower", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art6,
-        title: "The Stare", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art7,
-        title: "Dancing Beauty II", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art8,
-        title: "The Night King", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art9,
-        title: "Touka: The Rabbit", 
-        medium: "Watercolor on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art10,
-        title: "Raccoons", 
-        medium: "Color Pencil on Paper",
-        size: "Original (12'x16')"
-    },
-    {
-        source: art2,
-        title: "Anne Hathaway", 
-        medium: "Graphite on Paper",
-        size: "Original (12'x16')"
+    { 
+        source: art07,
+        description: "A watercolor painting of Tahoma (Mount Rainier) in the Pacific Northwest, from an aerial perspective. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
     }
 ]
