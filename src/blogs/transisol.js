fc8b16fa8fdbca5804b55f917c4ed644b96e6591
Pranshu Gupta <pranshug258@outlook.com>
2024-11-29 02:40:19 -0800

    added some content

--- src/blogs/transisol.js
+++ src/blogs/transisol.js
@@ -6,15 +6,20 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 const markdown = `
-## **Snapshot Isolation**
+## Transaction Anomalies
+1. A dirty read is a situation in which a transaction can read uncommitted changes from other trasactions.
+2. A non-repeatable read is a situation in which a transaction reads the same row multiple times and gets different values each time.
+3. A phantom read is a situation in which a transaction reads a set of rows that satisfy a certain condition, but when it reads the same set of rows again, it gets a different set of rows.
+4. A lost update is a situation in which two transactions update the same row, but one of the updates is lost.
+5. A dirty write is a situation in which a trsaction does a dirty read, modifies the value, and commits it.
+6. A write skew occurs when each individual transaction is consistent with the database constraints, but the combination of the transactions is not.
 
-## **Optimistic concurrency control**
-
-## **Multiversion concurrency control**
-
-## **Pessimistic concurrency control**
-
-## **Lock based concurrency control**
+A database system can prevent these anomalies by implemeting different levels of transaction isolation.
+## Snapshot Isolation
+## Optimistic concurrency control
+## Multiversion concurrency control
+## Pessimistic concurrency control
+## Lock based concurrency control
 `;
 
 export default class TransIsol extends React.Component {
@@ -29,11 +34,11 @@ export default class TransIsol extends React.Component {
                     <i className="fas fa-database bigger gt1"></i>
                 </div>
                 <h1 className="title">Transaction Isolation in Database Systems</h1>
-                <p>Pranshu Gupta, Oct 17, 2024</p>
-                <Sharer link={window.location.href} title={"Transaction Isolation in Database Systems"}></Sharer>
-                <br></br>
+                <p>Pranshu Gupta, Nov 28, 2024</p>
+                <Sharer className="sharer" link={window.location.href} title={"Transaction Isolation in Database Systems"}></Sharer>
                 <p className="introduction">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Transactional database systems allow different isolation levels. An isolation level specifies how and when parts of the transaction can and should become visible to other transactions that are being executed at the same time. This blog post will explore the different isolation levels and how they affect the transactional behavior of the database system.<br></br>
+                    However, to prevent incomplete or temporary writes from propagating over transaction boundaries, we need additional coordination and synchronization, which negatively impacts performance.
                 </p>
                 <hr style={{ backgroundColor: "white" }}></hr>
                 <ReactMarkdown remarkPlugins={[remarkGfm]} children={markdown}></ReactMarkdown>
@@ -42,7 +47,7 @@ export default class TransIsol extends React.Component {
                 <ol>
                     <li><a style={{ textAlign: "left", color: "black", fontSize: "inherit" }} href="https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/database-transactions-optimistic-concurrency">https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/database-transactions-optimistic-concurrency</a></li>
                 </ol>
-                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br>
             </div>
         )
     }
