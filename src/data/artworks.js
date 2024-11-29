92dc3ca2042f7d92676796a533f1cc13712d8506
Pranshu Gupta <pranshug258@outlook.com>
2024-11-29 12:05:57 -0800

    minor updates in description

--- src/data/artworks.js
+++ src/data/artworks.js
@@ -20,11 +20,11 @@ export let artList = [
     },
     { 
         source: art03,
-        description: "A watercolor painting of the Messier 106 galaxy, based on a photograph from the HST, NASA. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
+        description: "A watercolor painting of the Messier 106 galaxy, based on a photograph from the Hubble space telescope, courtesy NASA. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
     },
     { 
         source: art04,
-        description: "A watercolor painting of sunset at Long beach, Washington. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
+        description: "A watercolor painting of sunset at the Long beach, Washington. Painted on cold pressed Winsor & Newton paper, 12x16 inches.",
     },
     { 
         source: art07,
