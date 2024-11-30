f1b98a8b590921267d90c87409ba0934f54bec26
Pranshu Gupta <pranshug258@outlook.com>
2024-11-30 02:10:04 -0800

    completed isolation levels

--- src/blogs/transisol.js
+++ src/blogs/transisol.js
@@ -17,6 +17,28 @@ const markdown = `
 A database system can prevent these anomalies by implemeting different levels of transaction isolation. However, to prevent incomplete or temporary writes from propagating over transaction boundaries, we need additional coordination and synchronization, which negatively impacts performance.
 
 ## Isolation Levels
+The weakest isolation level is read uncommmitted, which allows dirty reads, non-repeatable reads, and phantom reads. The strongest isolation level is serializable, which prevents all anomalies. The other isolation levels are read committed, repeatable read, and snapshot isolation.
+
+### Read Committed
+In the read committed isolation level, a transaction can only read committed data. However, it is not guaranteed that the data will remain the same if the trasaction attempts to read the same data record once again at a later stage. This isolation level prevents dirty reads, but allows non-repeatable reads and phantom reads.
+
+### Repeatable Read
+In the repeatable read isolation level, a transaction can read the same data multiple times and get the same result each time. This isolation level prevents dirty reads and non-repeatable reads, but allows phantom reads.
+
+### Snapshot Isolation
+Some databases use snapshot isoolation, under which a transaction can observe the state changes performed by all transactions that were committed before the transaction started. The snapshot cannot change during transaction execution. The trasaction commits only if the values it has modified did not change while it was executing. Otherwise, the transaction is rolled back.
+
+If two transactions try to modify the same data, the second transaction will be rolled back. While dirty reads and non-repeatable reads are avoided, a write skew is possible under snapshot isolation. If two transactions read from local state, modify independent records, preserve local invariants, and commit, it is possible that the global invariants are violated.
+
+### Serializable
+Serializability is the strongest isolation level. A schedule is said to be **serial** when the transactions in it are executed completely independently and without any interleaving i.e. one transaction is executed completely before the next transaction starts. However, serializability significantly limits the system throughput and is not suitable for high-performance applications.
+
+Another approach is to use **serializable** schedules, which are not necessarily serial, but are guaranteed to be equivalent to some serial schedule. In other words, they could have interleaved transactions, but they produce the same result as if we executed a set of transactions one after another in some order. Serializable schedules are more flexible than serial schedules and can be used in high-performance applications.
+
+## Optimistic Concurrency Control
+Optimistic concurrency control assumes that conflicts are rare and that transactions can be executed concurrently without any coordination. When a transaction commits, the database system checks if the data it read has changed since it started. If the data has changed, the transaction is rolled back. 
+
+
 `;
 
 export default class TransIsol extends React.Component {
@@ -43,6 +65,7 @@ export default class TransIsol extends React.Component {
                 <h3 className="headings">References</h3>
                 <ol>
                     <li><a style={{ textAlign: "left", color: "black", fontSize: "inherit" }} href="https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/database-transactions-optimistic-concurrency">https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/database-transactions-optimistic-concurrency</a></li>
+                    <li>Database Internals by Alex Petrov | Chapter 5: Transaction Processing and Recovery</li>
                 </ol>
                 <br></br>
             </div>
