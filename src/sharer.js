dcb7792557f8c72c374739e7a5dab33688136b0b
Pranshu Gupta <pranshug258@outlook.com>
2024-11-30 12:57:50 -0800

    added threads

--- src/sharer.js
+++ src/sharer.js
@@ -27,7 +27,8 @@ export default class Sharer extends React.Component {
                 <ul id="sharerul">
                     <a onClick={this.copyToClipboard} style={{ color: "black", background: "none", border: "none", cursor: "pointer" }}><i className="fas fa-link sharerIcon"></i></a>
                     <a target="_blank" rel="noopener noreferrer" style={{ color: "black" }} href={"https://www.linkedin.com/sharing/share-offsite/?url=" + encodeURIComponent(this.props.link).replace("blog", "%3Fp%3Dblog")}><i className="fab fa-linkedin sharerIcon"></i></a>
-                    <a target="_blank" rel="noopener noreferrer" style={{ color: "black" }} href={"mailto:?subject=" + this.props.title + "&body=Check out this new article on Pranshu's blog:%0A" + this.props.link}><i className="fas fa-square-envelope sharerIcon"></i></a>
+                    <a target="_blank" rel="noopener noreferrer" style={{ color: "black" }} href={"mailto:?subject=" + this.props.title + "&body=Check out this new article on Pranshu's blog:%0A" + this.props.link}><i className="fas fa-envelope sharerIcon"></i></a>
+                    <a target="_blank" rel="noopener noreferrer" style={{ color: "black" }} href={"https://www.threads.net/intent/post?text=" + encodeURIComponent(this.props.link).replace("blog", "%3Fp%3Dblog")}><i className="fa-brands fa-threads  sharerIcon"></i></a>
                     <a target="_blank" rel="noopener noreferrer" style={{ color: "black" }} href={"https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(this.props.link)}><i className="fab fa-facebook sharerIcon"></i></a>
                     <a target="_blank" rel="noopener noreferrer" style={{ color: "black" }} href={"https://twitter.com/share?url=" + encodeURIComponent(this.props.link).replace("blog", "%3Fp%3Dblog")}><i className="fa-brands fa-x-twitter  sharerIcon"></i></a>
                     <a> {this.state.tooltipVisible && (
