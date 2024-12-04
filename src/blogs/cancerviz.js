993b1d3e8d747499c6cf304d7247f4b81d0ba7cd
Pranshu Gupta <pranshug258@outlook.com>
2024-12-03 22:17:44 -0800

    presenting as pagination

--- src/blogs/cancerviz.js
+++ src/blogs/cancerviz.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Sharer from '../sharer';
 import '../styles/fonts.css';
 import '../styles/blog.css';
-import cdclogo from '../images/cdclogo.png';
-import datagovlogo from '../images/datagov.png';
 import { cancerViz } from '../data/cancerviz';
 import { ResponsiveLine } from '@nivo/line'
 import { ResponsivePie } from '@nivo/pie'
@@ -34,9 +32,9 @@ const CancerIncidenceLineChart = ({ data }) => (
         axisBottom={{
             tickSize: 5,
             tickPadding: 5,
-            tickRotation: 0,
+            tickRotation: -90,
             legend: 'Year',
-            legendOffset: 36,
+            legendOffset: 54,
             legendPosition: 'middle',
             truncateTickAt: 0
         }}
@@ -60,7 +58,7 @@ const CancerIncidenceLineChart = ({ data }) => (
         colors={{ scheme: 'dark2' }}
         legends={[
             {
-                anchor: 'bottom-right',
+                anchor: 'top-right',
                 direction: 'column',
                 justify: false,
                 translateX: 100,
@@ -70,7 +68,7 @@ const CancerIncidenceLineChart = ({ data }) => (
                 itemTextColor: '#000',
                 itemWidth: 80,
                 itemHeight: 20,
-                itemOpacity: 0.75,
+                itemOpacity: 1,
                 symbolSize: 12,
                 symbolShape: 'circle',
                 symbolBorderColor: 'rgba(0, 0, 0, .5)',
@@ -83,7 +81,7 @@ const CancerIncidenceLineChart = ({ data }) => (
 const CancerIncidencePieChart = ({ data }) => (
     <ResponsivePie
         data={data}
-        margin={{ top: 40, right: 80, bottom: 100, left: 0 }}
+        margin={{ top: 10, right: 80, bottom: 100, left: 0 }}
         innerRadius={0.5}
         padAngle={0.7}
         cornerRadius={3}
@@ -112,9 +110,10 @@ const CancerIncidencePieChart = ({ data }) => (
                 ]
             ]
         }}
+        colors={{ scheme: 'tableau10' }}
         legends={[
             {
-                anchor: 'bottom-right',
+                anchor: 'top-right',
                 direction: 'column',
                 justify: false,
                 translateX: 80,
@@ -132,13 +131,94 @@ const CancerIncidencePieChart = ({ data }) => (
     />
 )
 
+const Visualisations = [
+    {
+        title: "yearly cancer incidents",
+        chart: <CancerIncidenceLineChart data={cancerViz.casesPerYear}></CancerIncidenceLineChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+    {
+        title: "leading cancer sites in the female population",
+        chart: <CancerIncidencePieChart data={cancerViz.casesBySiteFemale}></CancerIncidencePieChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+    {
+        title: "leading cancer sites in the male population",
+        chart: <CancerIncidencePieChart data={cancerViz.casesBySiteMale}></CancerIncidencePieChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+    {
+        title: "yearly incidents in leading sites in the female population",
+        chart: <CancerIncidenceLineChart data={cancerViz.femaleCasesPerYearBySite}></CancerIncidenceLineChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+    {
+        title: "yearly incidents in leading sites in the male population",
+        chart: <CancerIncidenceLineChart data={cancerViz.maleCasesPerYearBySite}></CancerIncidenceLineChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+    {
+        title: "incidents per age group",
+        chart: <CancerIncidenceLineChart data={cancerViz.totalCasesPerAgeGroupBySex}></CancerIncidenceLineChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+    {
+        title: "leading sites in the female population per age group",
+        chart: <CancerIncidenceLineChart data={cancerViz.femaleCasesPerAgeGroupBySite}></CancerIncidenceLineChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+    {
+        title: "leading cancer sites in the male population per age group",
+        chart: <CancerIncidenceLineChart data={cancerViz.maleCasesPerAgeGroupBySite}></CancerIncidenceLineChart>,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, nunc nec"
+    },
+];
+
 export default class CancerViz extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 1,
+            blogsPerPage: 1
+        };
+    }
+
+    handleNextPage = () => {
+        if (this.state.currentPage < this.totalPages()) {
+            this.setState({ currentPage: this.state.currentPage + 1 });
+        }
+    };
+
+    handlePreviousPage = () => {
+        if (this.state.currentPage > 1) {
+            this.setState({ currentPage: this.state.currentPage - 1 });
+        }
+    };
+
+    handlePageClick = (pageNumber) => {
+        this.setState({ currentPage: pageNumber });
+    };
+
+    totalPages = () => {
+        return Math.ceil(Visualisations.length / this.state.blogsPerPage);
+    };
+
     componentDidMount() {
         window.scrollTo(0, 0);
         document.title = "Exploring cancer dataset from CDC Wonder | blog by Pranshu Gupta";
     }
 
     render() {
+        const { currentPage, blogsPerPage } = this.state;
+        const indexOfLastBlog = currentPage * blogsPerPage;
+        const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
+        const currentVisualizations = Visualisations.slice(indexOfFirstBlog, indexOfLastBlog);
+
+        const pageNumbers = [];
+        for (let i = 1; i <= this.totalPages(); i++) {
+            pageNumbers.push(i);
+        }
+
         return (
             <div>
                 <div className="row bhead">
@@ -152,51 +232,44 @@ export default class CancerViz extends React.Component {
                     <br></br>
                 </p>
                 <hr style={{ backgroundColor: "white" }}></hr>
-                <h2>Introduction</h2>
-                <div style={{height: '300px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences per year</h6>
-                    <CancerIncidenceLineChart data={cancerViz.casesPerYear}></CancerIncidenceLineChart>
+                <h2>Key Cancer Statistics</h2>
+                <p>
+                    The visualizations below are based on the CDC WONDER Cancer Incidence dataset from 2001 to 2021, for all the states in the United States and Puerto Rico. We look at the cancer statistics the across different dimesnions such as sex, age group, calendar year, and leading cancer sites in the human body.
+                </p>
+                <br></br>
+                <div className="pagination" style={{ justifyContent: 'left' }}>
+                    <button style={{ marginRight: "10px", width: '100%' }} className="btn btn-light">
+                        {
+                            currentVisualizations.map((viz, index) => (
+                                <div key={index} className='paginationIcon'>
+                                    <h6 style={{ textAlign: 'left' }}>{viz.title}</h6>
+                                </div>
+                            ))
+                        }
+                    </button>
+                    <button style={{ marginRight: "10px" }} className="btn btn-dark" onClick={this.handlePreviousPage} disabled={currentPage === 1}>
+                        <i className="fas fa-arrow-left paginationIcon" title={`go to previous chart`}></i>
+                    </button>
+                    <button style={{ marginRight: "10px" }} className="btn btn-dark" onClick={this.handleNextPage} disabled={currentPage === this.totalPages()}>
+                        <i className="fas fa-arrow-right paginationIcon" title={`go to next chart`}></i>
+                    </button>
                 </div>
-                <div style={{height: '500px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences by sites (Female)</h6>
-                    <CancerIncidencePieChart data={cancerViz.casesBySiteFemale}></CancerIncidencePieChart>
-                </div>
-                <div style={{height: '500px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences by sites (Male)</h6>
-                    <CancerIncidencePieChart data={cancerViz.casesBySiteMale}></CancerIncidencePieChart>
-                </div>
-                <div style={{height: '500px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences (Female) per year by sites</h6>
-                    <CancerIncidenceLineChart data={cancerViz.femaleCasesPerYearBySite}></CancerIncidenceLineChart>
-                </div>
-                <div style={{height: '500px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences (Male) per year by sites</h6>
-                    <CancerIncidenceLineChart data={cancerViz.maleCasesPerYearBySite}></CancerIncidenceLineChart>
-                </div>
-                <div style={{height: '300px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences per age group by sex</h6>
-                    <CancerIncidenceLineChart data={cancerViz.totalCasesPerAgeGroupBySex}></CancerIncidenceLineChart>
-                </div>
-                <div style={{height: '500px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences (Female) per age group by sites</h6>
-                    <CancerIncidenceLineChart data={cancerViz.femaleCasesPerAgeGroupBySite}></CancerIncidenceLineChart>
-                </div>
-                <div style={{height: '500px', minWidth: '720px'}}>
-                    <h6 style={{textAlign: 'center'}}>Total cancer incidences (Male) per age group by sites</h6>
-                    <CancerIncidenceLineChart data={cancerViz.maleCasesPerAgeGroupBySite}></CancerIncidenceLineChart>
-                </div>
+                <br></br>
+                {
+                    currentVisualizations.map((viz, index) => (
+                        <div key={index}>
+                            <div style={{ height: '30rem' }}>
+                                {viz.chart}
+                            </div>
+                            <p>{viz.description}</p>
+                        </div>
+                    ))
+                }
                 <hr style={{ backgroundColor: "white" }}></hr>
                 <h5>Acknowledgements</h5>
                 <p>
-                    This article was made possible by the data collected and published for the people by the Centers for Disease Control and Prevention (CDC) and the data.gov initiative.<br></br>
-                    <code>
-                        <b>
-                            Citation: United States Cancer Statistics - Incidence: 1999 - 2021, WONDER Online Database. United States Department of Health and Human Services, Centers for Disease Control and Prevention and National Cancer Institute; 2023 submission; 2024 release. Accessed at http://wonder.cdc.gov/cancer-v2021.html on Dec 1, 2024.
-                        </b>
-                    </code>
+                    United States Cancer Statistics - Incidence: 1999 - 2021, WONDER Online Database. United States Department of Health and Human Services, Centers for Disease Control and Prevention and National Cancer Institute; 2023 submission; 2024 release. Accessed at http://wonder.cdc.gov/cancer-v2021.html on Dec 1, 2024.
                 </p>
-                <img src={cdclogo} style={{ height: '40px', marginRight: '10px' }} alt="CDC Logo" className="img-fluid"></img>
-                <img src={datagovlogo} style={{ height: '40px', marginRight: '10px' }} alt="data gov Logo" className="img-fluid"></img>
                 <hr style={{ backgroundColor: "white" }}></hr>
             </div>
         )
